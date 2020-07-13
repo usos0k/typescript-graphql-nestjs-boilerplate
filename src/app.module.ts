f92051b2e41a0ddbf5ef5984ee0b09a184af3187
ggomma <ggomma@gmail.com>
2020-07-13 18:11:06 +0900

    Initial

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
+})
+export class AppModule {}
