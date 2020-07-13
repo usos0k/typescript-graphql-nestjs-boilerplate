66555237bb27fc84a0e51897906acd54d24275db
ggomma <ggomma@gmail.com>
2020-07-13 22:17:26 +0900

    Implement tasks structure

---
+++ src/shared/tasks/index.ts
@@ -0,0 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { CronJob } from 'cron';
+
+export const cron = (): void => {
+  const job = new CronJob({
+    cronTime: '0 0 0 * *',
+    onTick: () => {
+      Logger.debug('cron job completed', 'Cron', false);
+    },
+    timeZone: 'Asia/Seoul',
+  });
+
+  job.start();
+};
