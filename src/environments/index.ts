42dcc2c6132cda8ffcb01fe56f642e0dde70549c
ggomma <ggomma@gmail.com>
2020-07-16 15:45:15 +0900

    Specify node_env using dotenv

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,6 +1,13 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+const env = {
+  development: 'dev',
+  production: 'prod',
+  local: 'local',
+  test: 'test',
+}[process.env.NODE_ENV || 'local'];
+
+dotenv.config({ path: `.env.${env}` });
 
 // environment
 export const NODE_ENV: string = process.env.NODE_ENV || 'development';
@@ -11,7 +18,11 @@ export const PORT: number = +`${process.env.PORT}` || 3000;
 export const RATE_LIMIT_MAX: number = +`${process.env.RATE_LIMIT_MAX}` || 10000;
 
 // DB
+export const MYSQL_HOST: string = process.env.MYSQL_HOST || 'localhost';
+export const MYSQL_PORT: number = +`${process.env.MYSQL_PORT}` || 3306;
 export const MYSQL_DB: string = process.env.MYSQL_DB || '';
+export const MYSQL_USERNAME: string = process.env.MYSQL_USERNAME || '';
+export const MYSQL_PASSWORD: string = process.env.MYSQL_PASSWORD || '';
 
 // jsonwebtoken
 export const ISSUER: string = process.env.ISSUER || 'SKELETON';
