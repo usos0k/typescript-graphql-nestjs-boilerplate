d32f8bbdd1a31175f08d2c13199cbe59d1966dc4
junhapark <5709psy@gmail.com>
2020-08-03 08:29:04 +0900

    plop update

--- plopfile.js
+++ plopfile.js
@@ -20,7 +20,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/index.ts',
-          templateFile: 'plop-templates/template/index.txt',
+          templateFile: 'plop-templates/module/index.txt',
           data : {
             name : data.name,
             pluralName : pluralize(data.name),
@@ -30,7 +30,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/' + pluralize(data.name) +'.graphql',
-          templateFile: 'plop-templates/template/templates.txt',
+          templateFile: 'plop-templates/module/templates.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -41,7 +41,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/' + pluralize(data.name) +'.module.ts',
-          templateFile: 'plop-templates/template/templates.module.txt',
+          templateFile: 'plop-templates/module/templates.module.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -52,7 +52,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/' + pluralize(data.name) +'.service.ts',
-          templateFile: 'plop-templates/template/templates.service.txt',
+          templateFile: 'plop-templates/module/templates.service.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -63,7 +63,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/' + pluralize(data.name) +'.resolver.ts',
-          templateFile: 'plop-templates/template/templates.resolver.txt',
+          templateFile: 'plop-templates/module/templates.resolver.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -74,7 +74,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/repositories/' + data.name +'.repository.ts',
-          templateFile: 'plop-templates/template/repositories/template.repository.txt',
+          templateFile: 'plop-templates/module/repositories/template.repository.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -84,8 +84,8 @@ module.exports = function (plop) {
         },
         {
           type: 'add',
-          path: 'src/modules/' + pluralize(data.name) + '/entities/' + pluralize(data.name) +'.entity.ts',
-          templateFile: 'plop-templates/template/entities/templates.entity.txt',
+          path: 'src/modules/' + pluralize(data.name) + '/entities/' + data.name +'.entity.ts',
+          templateFile: 'plop-templates/module/entities/templates.entity.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -96,7 +96,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/dto/create-' + data.name +'.dto.ts',
-          templateFile: 'plop-templates/template/dto/create-template.dto.txt',
+          templateFile: 'plop-templates/module/dto/create-template.dto.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -107,7 +107,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/dto/find-' + data.name +'.dto.ts',
-          templateFile: 'plop-templates/template/dto/find-template.dto.txt',
+          templateFile: 'plop-templates/module/dto/find-template.dto.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -118,7 +118,7 @@ module.exports = function (plop) {
         {
           type: 'add',
           path: 'src/modules/' + pluralize(data.name) + '/dto/find-' + pluralize(data.name) +'.dto.ts',
-          templateFile: 'plop-templates/template/dto/find-templates.dto.txt',
+          templateFile: 'plop-templates/module/dto/find-templates.dto.txt',
           data : {
             name : data.name,
             capName : capitalizeFirstLetter(data.name),
@@ -129,7 +129,7 @@ module.exports = function (plop) {
       );
       
       return actions
-    
+      
     }
   });
 };
