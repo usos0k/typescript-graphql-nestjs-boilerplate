bc745828526f79d56c10ff74a03ce7462bd81131
junhapark <5709psy@gmail.com>
2021-07-01 12:38:18 +0900

    User Entity ㅅㅜ정

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { generateToken } from '@/auth';
+import { ValidationEntity } from '@/common/entities';
 import { BCRYPT_SALT } from '@/environments';
 import bcrypt from 'bcrypt';
+import { Expose, plainToClass } from 'class-transformer';
 import { IsBoolean, IsEmail, IsNotEmpty, Length } from 'class-validator';
 import {
   BaseEntity,
@@ -8,15 +10,25 @@ import {
   Column,
   Entity,
   ObjectIdColumn,
+  Unique,
 } from 'typeorm';
+import * as uuid from 'uuid';
 
 import { UserRO } from '../interfaces';
 
+export enum IsVerified {
+  WAITING = 'WAITING',
+  APPROVED = 'APPROVED',
+  DECLINED = 'DECLINED',
+}
+
 @Entity('users')
-export class UserEntity extends BaseEntity {
+@Unique(['email'])
+export class UserEntity extends ValidationEntity {
   @ObjectIdColumn()
   _id: string;
 
+  @Expose()
   @Column()
   @IsEmail()
   email!: string;
@@ -25,14 +37,52 @@ export class UserEntity extends BaseEntity {
   @IsNotEmpty()
   password!: string;
 
-  @Column({ default: false })
-  @IsBoolean()
-  isVerified!: boolean;
-
+  @Expose()
   @Column()
   @IsNotEmpty()
   @Length(1, 10)
   name!: string;
+
+  @Column({ nullable: true })
+  comment: string;
+
+  @Column('enum', { enum: IsVerified, default: IsVerified.WAITING })
+  isVerified!: IsVerified;
+
+  @Column({ default: false })
+  isEmailVerified!: boolean;
+
+  @Column({ default: false })
+  isDeleted!: boolean;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt!: string;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt!: string;
+
+  constructor(userEntity?: Partial<UserEntity>) {
+    super();
+    if (userEntity) {
+      Object.assign(
+        this,
+        plainToClass(UserEntity, userEntity, {
+          // excludeExtraneousValues: true,
+        }),
+      );
+      this._id = this._id || uuid.v1();
+      // this.createdAt = this.createdAt || +new Date();
+      // this.updatedAt = +new Date();
+      // this.isVerified = this.isActive !== undefined ? this.isActive : false;
+      // this.isActive = this.isActive !== undefined ? this.isActive : true;
+    }
+  }
 
   @BeforeInsert()
   async hashPassword(): Promise<void> {
@@ -46,7 +96,6 @@ export class UserEntity extends BaseEntity {
   toResponseObject(showToken = true): UserRO {
     const responseObject: UserRO = {
       ...this,
-      id: `${this._id}`,
       token: undefined,
     };
 
@@ -57,7 +106,7 @@ export class UserEntity extends BaseEntity {
 
   private get token(): string {
     return generateToken({
-      id: `${this._id}`,
+      _id: `${this._id}`,
       type: 'accessToken',
     });
   }
